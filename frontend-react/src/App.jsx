d0b29c86d0060f71bd209dbac03c15cdb492c16e
kirankar123 <161498919+kirankar123@users.noreply.github.com>
2025-09-05 16:33:08 +0530

    Initialize frontend React application with Vite, create registration form component, and set up basic styling and validation.

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RegistrationForm from "./registration";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "90px" }}>
+        Registration Form
+      </h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
